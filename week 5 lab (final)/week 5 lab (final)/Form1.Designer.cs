fdd51c63134f7bc16df0bfee430db47662b03820
Shahrair Sojib <you@example.com>
2021-04-13 03:51:38 +0600

    week5 labtask

---
+++ week 5 lab (final)/week 5 lab (final)/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace week_5_lab__final_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textb1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textb2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.viewDetails = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.viewDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name :";
+            // 
+            // textb1
+            // 
+            this.textb1.Location = new System.Drawing.Point(177, 56);
+            this.textb1.Name = "textb1";
+            this.textb1.Size = new System.Drawing.Size(159, 20);
+            this.textb1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Course Code  :";
+            // 
+            // textb2
+            // 
+            this.textb2.Location = new System.Drawing.Point(177, 122);
+            this.textb2.Name = "textb2";
+            this.textb2.Size = new System.Drawing.Size(159, 20);
+            this.textb2.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(426, 58);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(426, 121);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // viewDetails
+            // 
+            this.viewDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewDetails.Location = new System.Drawing.Point(33, 185);
+            this.viewDetails.Name = "viewDetails";
+            this.viewDetails.Size = new System.Drawing.Size(468, 233);
+            this.viewDetails.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 441);
+            this.Controls.Add(this.viewDetails);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textb2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textb1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.viewDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textb1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textb2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView viewDetails;
+    }
+}
+
---
+++ week 5 lab (final)/week 5 lab (final)/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace week_5_lab__final_
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowAllCourse();
+        }
+
+        private void ShowAllCourse()
+        {
+            SqlConnection con = new SqlConnection(@"Server=SHAHRAIRSOJIBPC\SQLEXPRESS;Database=db_finel;Integrated Security=true;");
+
+            if (con == null)
+            {
+                MessageBox.Show("Connection Failed", "Database Connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            con.Open();
+
+            string query = "SELECT * FROM coures";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader data = cmd.ExecuteReader();
+
+            List<Course> courses = new List<Course>();
+
+            while(data.Read())
+            {
+                Course course = new Course();
+
+                course.Id = data.GetInt32(data.GetOrdinal("id"));
+                course.CourseName  = data.GetString(data.GetOrdinal("name"));
+                course.CourseCode = data.GetString(data.GetOrdinal("code"));
+
+                courses.Add(course);
+            }
+
+            con.Close();
+
+            viewDetails.DataSource = courses;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string courseName = textb1.Text.Trim();
+            string courseCode = textb2.Text.Trim();
+
+            SqlConnection con = new SqlConnection(@"Server=SHAHRAIRSOJIBPC\SQLEXPRESS;Database=db_finel;Integrated Security=true;");
+
+            if (con == null)
+            {
+                MessageBox.Show("Connection Failed", "Database Connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            con.Open();
+
+            string query = String.Format("INSERT INTO coures VALUES('{0}', '{1}')", courseName, courseCode);
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            int row = cmd.ExecuteNonQuery();
+
+            if(row == 0)
+            {
+                MessageBox.Show("Insertion Failed", "Database Connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                con.Close();
+                return;
+            }
+
+            con.Close();
+
+            ShowAllCourse();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            ShowAllCourse();
+        }
+    }
+}
